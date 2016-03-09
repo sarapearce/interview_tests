8e6e90d96b2d0c82c55dd1e4b38ad8274d3f25f9
Sara Pearce <sarapearce3.14@gmail.com>
2016-03-09 14:40:10 -0600

    Update shipping.js

--- shipping.js
+++ shipping.js
@@ -1,14 +1,16 @@
+
+//returns shortest shipping time based on product_id and customer zipcode
+
 $(document).ready(function () {
     getFastestGrid(["KC2000", "TWR1501"]);
 });
 
-this.pWarehouses = [];
+var pWarehouses = [];
 pWarehouses["KC2000"] = ["78756", "90210"];
 pWarehouses["TWR1501"] = ["10001", "90210"];
 pWarehouses["ES1500"] = ["10001"];
 
 var numCalled = 0;
-
 
 this.Warehouse_1_Zip = "78756";
 this.Warehouse_2_Zip = "90210";
@@ -54,11 +56,10 @@ function getFastest(ProductId) {
 }
 
 function getFastestGrid(ProductIds) {
-    
     var czip = getCZip();
-    var warehouse_times = {};
-    var values = [];
-    var fastest_times = {};
+    var warehouse_times = { };
+    var values = [ ];
+    var fastest_times = { };
     if (ProductIds.length > 1) {
 	ProductIds.forEach(function (product_id) {
 	    var delivery_time = getDeliveryTime(product_id);
@@ -73,30 +74,32 @@ function getFastestGrid(ProductIds) {
 	var fastest_time = getFastest(czip);
 	fastest_times[ProductIds] = fastest_time;
     }
-    //console.log(fastest_times);
     return fastest_times;
-
+  
 }
 
 
 function getDeliveryTime(ProductId) {
-
-    var delivery_times = {};
+    var delivery_times = { };
     var czip = getCZip();
     var warehouse_zips = getProductWarehouses(ProductId);
-
-    warehouse_zips.forEach(function (wzip) {
-	var time = calculateTime(czip, wzip);
-	delivery_times.push(time);
-    });
-
+    
+    if (warehouse_zips.length > 1) {
+	warehouse_zips.forEach(function (wzip) {
+	    if (Object.keys(delivery_times).indexOf(wzip) === -1) {
+		delivery_times[wzip] = calculateTime(czip, wzip);
+	    }
+	});
+    } else {
+	//warehouse_zips has only 1 value.
+	var wzip = warehouse_zips;
+	if (Object.keys(delivery_times).indexOf(wzip) === -1) {
+	    delivery_times[wzip] = calculateTime(czip, wzip);
+	}
+    }
     return delivery_times;
-
 }
 
 Array.min = function (array) {
-    
     return Math.min.apply(Math, array);
-    
 };
-
