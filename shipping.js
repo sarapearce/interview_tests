c04344b1c53047b2057c374d94274f5e9b3d6ac1
Sara Pearce <sarapearce3.14@gmail.com>
2016-03-12 15:25:14 -0600

    Update shipping.js

--- shipping.js
+++ shipping.js
@@ -3,24 +3,25 @@
 
 $(document).ready(function () {
     getFastestGrid(["KC2000", "TWR1501"]);
+//    getFastestGrid(["KC2000"]);
 });
 
-var pWarehouses = [];
+//global storage for existing warehouse zips and delivery_times
+this.existing_warehouse_data = [];
+
+this.pWarehouses = [];
 pWarehouses["KC2000"] = ["78756", "90210"];
 pWarehouses["TWR1501"] = ["10001", "90210"];
 pWarehouses["ES1500"] = ["10001"];
 
 var numCalled = 0;
 
-this.Warehouse_1_Zip = "78756";
-this.Warehouse_2_Zip = "90210";
-this.Warehouse_3_Zip = "10001";
-
 function getCZip() {
     return "78703";
 }
 
 function calculateTime(zip1, zip2) {
+
     numCalled++;
     if (zip2 == "90210") {
 	return 5;
@@ -56,50 +57,75 @@ function getFastest(ProductId) {
 }
 
 function getFastestGrid(ProductIds) {
-    var czip = getCZip();
-    var warehouse_times = { };
-    var values = [ ];
-    var fastest_times = { };
-    if (ProductIds.length > 1) {
-	ProductIds.forEach(function (product_id) {
-	    var delivery_time = getDeliveryTime(product_id);
-	    for (var key in delivery_time) {
-		values.push(parseInt(delivery_time[key]));
-	    }
-	    var fastest_time = Array.min(values);
-	    fastest_times[product_id] = fastest_time;
-	});
-    } else {
-	//only 1 product id
-	var fastest_time = getFastest(czip);
-	fastest_times[ProductIds] = fastest_time;
-    }
+
+    var fastest_times = [];
+
+    ProductIds.forEach(function (product_id) {
+	var values = [];
+	var delivery_times = getDeliveryTimes(product_id);
+
+	for (var key in delivery_times) {
+	    //look at right side (time) of zip_time element in the array.
+	    var time = delivery_times[key].split('_')[1];
+	    values.push(Number.parseInt(time));
+	}
+
+	var fastest_time = Array.min(values);
+	var product_delivery_time = {};
+	product_delivery_time[product_id] = fastest_time;
+	fastest_times.push(product_delivery_time);
+
+    });
+    
     return fastest_times;
-  
+
 }
 
+function getDeliveryTimes(ProductId) {
 
-function getDeliveryTime(ProductId) {
-    var delivery_times = { };
-    var czip = getCZip();
     var warehouse_zips = getProductWarehouses(ProductId);
-    
-    if (warehouse_zips.length > 1) {
-	warehouse_zips.forEach(function (wzip) {
-	    if (Object.keys(delivery_times).indexOf(wzip) === -1) {
-		delivery_times[wzip] = calculateTime(czip, wzip);
-	    }
-	});
-    } else {
-	//warehouse_zips has only 1 value.
-	var wzip = warehouse_zips;
-	if (Object.keys(delivery_times).indexOf(wzip) === -1) {
-	    delivery_times[wzip] = calculateTime(czip, wzip);
+    var delivery_times = [];
+
+    warehouse_zips.forEach(function (wzip) {
+	//check to prevent redudant calls to calculateTime for same warehouse.
+	if (getExistingWZipTimes(wzip).length === 0) {
+	    var time = calculateTime(getCZip(), wzip);
+	    //create zip_time element and push 
+	    delivery_times.push(wzip + '_' + time);
+	    this.existing_warehouse_data.push(wzip + '_' + time);
+	} else {
+	    //we have a warehouse zip_time already stored, get it from there.
+	    var wzip_times = getExistingWZipTimes(wzip);
+	    //looping not quite necessary in this use case, but as more warehouses hold a product, we will have >1 in the array.
+	    wzip_times.forEach(function (wzip_time) {
+		delivery_times.push(wzip_time);
+		this.existing_warehouse_data.push(wzip_time);
+	    });
 	}
-    }
+    });
+
     return delivery_times;
+
+}
+
+function getExistingWZipTimes(zip) {
+
+    var wzip_times = [];
+
+    this.existing_warehouse_data.forEach(function (wzip_time) {
+	//take left side (zip) of zip_time element.
+	var wzip = wzip_time.split('_')[0];
+	if (wzip === zip) {
+	    wzip_times.push(wzip_time);
+	}
+    });
+
+    return wzip_times;
+
 }
 
 Array.min = function (array) {
+
     return Math.min.apply(Math, array);
+
 };
